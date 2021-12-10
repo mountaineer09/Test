aff61c476333bf54a47aaa687b17cc5694b84cf1
DESKTOP-EHREQN8\seora <lum.damount09@gmail.com>
2021-12-10 17:59:13 +0100

    no message

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,19 +2,11 @@ import Link from 'next/link'
 
 const Navbar= () => {
     return (
-<nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div className="navbar-nav">
-        <a className="nav-link active" aria-current="page" href="#">Home</a>
-        <a className="nav-link" href="#">Features</a>
-        <a className="nav-link" href="#">Pricing</a>
-      </div>
-    </div>
+<nav>
+  <div>
+  <h1>About</h1>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the </p>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the </p>
   </div>
 </nav>
     );
