ee6f1959e3ecae9f4af1346d05bbc2c8ea0ba6fc
DESKTOP-EHREQN8\seora <lum.damount09@gmail.com>
2021-12-13 09:28:56 +0100

    monday1

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,7 +3,7 @@ const About= () => {
      <div>
         <h1>About</h1>
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the </p>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the </p>
+        <p>OK TEST Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the </p>
      </div>
     );
 }
