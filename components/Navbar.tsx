ee7a535546496caa4dd78ba71026c511a9a2be22
DESKTOP-EHREQN8\seora <lum.damount09@gmail.com>
2021-12-14 10:56:09 +0100

    modif tarifs 2

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -19,20 +19,20 @@ const Navbar= () => {
         className="m-2 w-10 sm:w-20 self-center"                                          
         src="\lodge.PNG" width="45" alt=""
       /></a>
-    <Link href={'/index'}><a className="navbar-brand" href="#">Chalet Lum D’Amont</a></Link>
+    <Link href="/"><a className="navbar-brand" href="#">Chalet Lum D’Amont</a></Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNavDropdown">
       <ul className="navbar-nav">
         <li className="nav-item">
-        <Link href={'/index'}><a className="nav-link active" aria-current="page" href="#">Home</a></Link>
+        <Link href="/"><a className="nav-link active" aria-current="page" href="#">Home</a></Link>
         </li>
         <li className="nav-item">
-        <Link href={'/home'}><a className="nav-link" href="#">Lum d’Amont</a></Link>
+        <Link href="/home"><a className="nav-link" href="#">Lum d’Amont</a></Link>
         </li>
         <li className="nav-item">
-          <Link href={'/tarifs'}><a className="nav-link" href="#">Tarifs</a></Link>
+          <Link href='/tarifs'><a className="nav-link" href="#">Tarifs</a></Link>
         </li>
         <li className="nav-item dropdown">
           <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
--- pages/tarifs.tsx
+++ pages/tarifs.tsx
@@ -1,107 +1,119 @@
 import Navbar from "../components/Navbar";
 
-const Tarifs= () => {
+const Tarifs = () => {
     return (
         <div>
             <Navbar></Navbar>
-        <div className="container">
-      
-        <div className="row justify-content-lg-center">
-          <div className="col-lg-12">
-            <h2 className="text-center">Tarifs</h2>
-            <div className="separator"></div>
-            
-          
-          
-<div className="row">
-  <div className="col-md-7"><p className="large"><div className="row">
-<div className="col-md-12">
-<p>Pour toute demande d'information ou questions, vous pouvez nous contacter à l'adresse contact@lum-damont.com<br />
-<span>Télécharger le bulletin d'inscription</span><br />
-<span></span><br />
-<span></span><br />
-<span></span></p>
+            <div className="col">
+                1 of 2
+            </div>
+            <div className="col">
+                2 of 2
+            </div>
 
-<p><span><strong></strong></span><br />
-<span></span></p>
+            <div className="container">
 
-<div className="row two-col">
-<div className="col-md-6 col-1">
-<p><strong>Conditions de réservation:</strong><br />
-- Bons CAF et Chèques vacances acceptés<br />
-- Cotisation annuelle obligatoire de 1.00 euro par adulte ou de 15.00 euros par groupe constitué<br />
-- Ne sont prises en compte uniquement les réservations accompagnées d'un acompte de 25,00% du montant total arrondi à  l'euro supérieur<br />
-- Les tarifs indiqués ne comprennent pas la taxe de séjour qui est calculée sur la base des règles en vigueur<br />
-- Une caution de 300,00 euros est demandée en début séjour et encaissée seulement en cas de dégâts<br />
-</p>
-</div>
 
-<div className="col-md-6 col-2">
-<p><br />
-<br />
-<br />
-</p>
-</div>
-</div>
 
-<blockquote>
-<p>&Agrave; votre arrivée, vos lits seront préparés, seuls les rangements attendront vos effets et le réfrigérateur vos courses...</p>
-</blockquote>
-</div>
-</div>
-</p></div>
-  <div className="col-md-5">
-  
-<div className="table-responsive well2">               
- <table className="table table-bordered table-striped">
-    <thead>
-      <tr>
-        <th className="text-center">Adultes</th>
 
-      </tr>
-    </thead>
-    <tbody>
-      
-      <tr>
-        <td className="text-center">Tarifs famille</td>
-      </tr>
-      
-      <tr>
-        <td className="text-center">Adultes</td>
-        <td className="text-center">47,00 &euro;</td>
-      </tr>
-      
-      <tr>
-        <td className="text-center">Enfants de moins de 13 ans</td>
-        <td className="text-center">37,00 &euro;</td>
-      </tr>
-      
-      <tr>
-        <td className="text-center">Enfants de moins de 5 ans</td>
-        <td className="text-center">Gratuit</td>
-      </tr>
 
-    </tbody>
-  </table>
-  
-  </div>  
-  
-  
-  </div>
-</div>         
-          
-          
-          
-          
-          </div>
+                <div className="row justify-content-lg-center">
+                    <div className="col-lg-12">
+                        <h2 className="text-center">Tarifs</h2>
+                        <div className="separator"></div>
+
+
+
+                        <div className="row">
+                            <div className="col-md-7"><p className="large"><div className="row">
+                                <div className="col-md-12">
+                                    <p>Pour toute demande d'information ou questions, vous pouvez nous contacter à l'adresse contact@lum-damont.com<br />
+                                        <span>Télécharger le bulletin d'inscription</span><br />
+                                        <span></span><br />
+                                        <span></span><br />
+                                        <span></span></p>
+
+                                    <p><span><strong></strong></span><br />
+                                        <span></span></p>
+
+
+
+                                    <p><strong>Conditions de réservation:</strong><br />
+                                        - Bons CAF et Chèques vacances acceptés<br />
+                                        - Cotisation annuelle obligatoire de 1.00 euro par adulte ou de 15.00 euros par groupe constitué<br />
+                                        - Ne sont prises en compte uniquement les réservations accompagnées d'un acompte de 25,00% du montant total arrondi à l'euro supérieur<br />
+                                        - Les tarifs indiqués ne comprennent pas la taxe de séjour qui est calculée sur la base des règles en vigueur<br />
+                                        - Une caution de 300,00 euros est demandée en début séjour et encaissée seulement en cas de dégâts<br />
+                                    </p>
+
+
+
+
+                                    <blockquote>
+                                        <p>&Agrave; votre arrivée, vos lits seront préparés, seuls les rangements attendront vos effets et le réfrigérateur vos courses...</p>
+                                    </blockquote>
+                                </div>
+                            </div>
+                            </p></div>
+                            <div className="col-md-5">
+
+                                <div className="table-responsive well2">
+                                    <table className="table table-bordered table-striped">
+                                        <thead>
+                                            <tr>
+                                                <th className="text-center">Adultes</th>
+
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+
+                                            <tr>
+                                                <td className="text-center">Tarifs famille</td>
+                                            </tr>
+
+                                            <tr>
+                                                <td className="text-center">Adultes</td>
+                                                <td className="text-center">47,00 &euro;</td>
+                                            </tr>
+
+                                            <tr>
+                                                <td className="text-center">Enfants de moins de 13 ans</td>
+                                                <td className="text-center">37,00 &euro;</td>
+                                            </tr>
+
+                                            <tr>
+                                                <td className="text-center">Enfants de moins de 5 ans</td>
+                                                <td colSpan={2} className="text-center">Gratuit</td>
+                                            </tr>
+                                            <tr>
+
+                                                <td colSpan={2} className="text-center">Gratuit</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+
+                                </div>
+
+
+                            </div>
+                        </div>
+
+
+
+
+                    </div>
+                </div>
+
+
+
+
+
+            </div>
         </div>
-    
-       
-    
-    
-        
-      </div>
-      </div>
+
+
+
+
     );
 }
 
